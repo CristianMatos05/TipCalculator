eda3dae52171e64c3713880f1c6be3f8f6dfdf87
CRISTIAN ALONSO MATOS XOL <24470034.campeche.tecnm.mx>
2025-10-17 10:55:55 -0600

    SE AGREGA EL COMPORTAMIENTO DE INTERFAZ DE USUARIO

--- TipCalculator.Gui/MainPage.xaml.cs
+++ TipCalculator.Gui/MainPage.xaml.cs
@@ -7,9 +7,72 @@
         public MainPage()
         {
             InitializeComponent();
+            LIMPIAR();
+        }
+
+        private void ButtonCALCULAR(object sender, EventArgs e)
+        {
+
+            decimal importedelcosumo = Convert.ToDecimal(IMPORTEDELCONSUMOENTRY.Text);
+            decimal numerodepersonas = Convert.ToDecimal(NUMERODEPERSONASENTRY.Text);
+            decimal porcentajedepropina = Convert.ToDecimal(PORCENTAJEDEPROPINA.Text);
+
+            decimal propinaTotal = CalcularImporteDePropinaTotal(importedelcosumo, porcentajedepropina);
+            decimal propinaPorPersona = CalcularPropinaPorPersona(propinaTotal, numerodepersonas);
+            decimal importeTotal = CalcularImporteTotal(importedelcosumo, propinaTotal);
+            decimal importeTotalPorPersona = CalcularImporteTotalPorPersona(importeTotal, numerodepersonas);
+
+
+            IMPORTEPROPINATOTALLABEL.Text = $"{propinaTotal:C}";
+            PROPINAPORPERSONALABEL.Text = $"{propinaPorPersona:C}";
+            IMPORTETOLALABEL.Text = $"{importeTotal:C}";
+            IMPORTETOTALPORPERSONALABEL.Text = $"{importeTotalPorPersona:C}";
+
+
+        }
+        decimal CalcularImporteDePropinaTotal(decimal importeConsumo, decimal porcentajePropina)
+        {
+            return (porcentajePropina / 100) * importeConsumo;
+        }
+
+        decimal CalcularPropinaPorPersona(decimal propinaTotal, decimal numeroPersonas)
+        {
+            return propinaTotal / numeroPersonas;
+        }
+
+        decimal CalcularImporteTotal(decimal importeConsumo, decimal propinaTotal)
+        {
+            return importeConsumo + propinaTotal;
+        }
+
+        decimal CalcularImporteTotalPorPersona(decimal importeTotal, decimal numeroPersonas)
+        {
+            return importeTotal / numeroPersonas;
         }
 
 
+
+
+        void LIMPIAR()
+        {
+            IMPORTEPROPINATOTALLABEL.Text = string.Empty;
+            PROPINAPORPERSONALABEL.Text = string.Empty;
+            IMPORTETOLALABEL.Text = string.Empty;
+            IMPORTETOTALPORPERSONALABEL.Text= string.Empty;
+
+            IMPORTEDELCONSUMOENTRY.Text= string.Empty;
+            NUMERODEPERSONASENTRY.Text = string.Empty;
+            PORCENTAJEDEPROPINA.Text = string.Empty;
+
+        }
+        private void ButtonLIMPIAR(object sender, EventArgs e)
+        {
+
+
+            LIMPIAR();
+
+
+        }
     }
 
 }
