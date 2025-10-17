d37aeaa0d540015acaf79d84def87d1673afec24
CRISTIAN ALONSO MATOS XOL <24470034.campeche.tecnm.mx>
2025-10-17 09:33:20 -0600

    PAGINA EN BLANCO

---
+++ TipCalculator.Gui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TipCalculator.Gui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
